293730bacf599fbb2818ee3578a1a135e07a0b1b
Bryan L. Venable <bryanlvenable@gmail.com>
2015-07-17 18:15:56 -0700

    added injector to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,20 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    inject = require('gulp-inject'),
+    angularFilesort = require('gulp-angular-filesort');
 
-gulp.task('default', function() {
+
+gulp.task('default', function () {
   // Place code for default task here
 });
+
+gulp.task('index', function () {
+  var target = gulp.src('./client/index.html');
+  var sources = gulp.src([
+    './client/app/**/*.js',
+    './client/app/**/*.css',
+    './client/bower_components/**/*.min.js'])
+    .pipe(angularFilesort());
+
+  return target.pipe(inject(sources))
+    .pipe(gulp.dest('./client'));
+})
